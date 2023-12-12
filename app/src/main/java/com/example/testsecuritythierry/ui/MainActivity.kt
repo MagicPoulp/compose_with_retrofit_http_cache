4f347ed6d14ad3cd0a12078a449b959f50c9dae3
Thierry Vilmart <3144141+MagicPoulp@users.noreply.github.com>
2023-12-12 18:24:47 +0100

    fix a timing issue since the prefetching can take a long time

--- app/src/main/java/com/example/testsecuritythierry/ui/MainActivity.kt
+++ app/src/main/java/com/example/testsecuritythierry/ui/MainActivity.kt
@@ -4,9 +4,15 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
+import androidx.compose.foundation.layout.Row
+import androidx.compose.runtime.getValue
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import com.example.testsecuritythierry.ui.components.NavigationScreen
+import com.example.testsecuritythierry.ui.components.ProgressIndicator
 import com.example.testsecuritythierry.ui.components.UiStateScreen
 import com.example.testsecuritythierry.ui.theme.TestSecurityThierryTheme
 import com.example.testsecuritythierry.ui.view_models.MainActivityViewModel
+import com.example.testsecuritythierry.ui.view_models.UiState
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -18,7 +24,15 @@ class MainActivity : ComponentActivity() {
 
         setContent {
             TestSecurityThierryTheme {
-                UiStateScreen(activity = this)
+                // this extra UIState is because we want to prefetch the data before going to the
+                // UiStateScreen
+                val state by mainActivityViewModel.uiState.collectAsStateWithLifecycle()
+                when (state) {
+                    UiState.Filled -> UiStateScreen(activity = this)
+                    else -> Row {
+                        ProgressIndicator()
+                    }
+                }
             }
         }
 
