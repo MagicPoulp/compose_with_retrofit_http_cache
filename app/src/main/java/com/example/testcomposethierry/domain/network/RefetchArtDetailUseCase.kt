6dd4527afa2c65580fd784207f034b976dcf3d44
Thierry Vilmart <thierry.vilmart@REPLACE.com>
2024-01-24 22:43:48 +0100

    create more use cases

---
+++ app/src/main/java/com/example/testcomposethierry/domain/network/RefetchArtDetailUseCase.kt
@@ -0,0 +1,28 @@
+package com.example.testcomposethierry.domain.network
+
+import androidx.paging.compose.LazyPagingItems
+import com.example.testcomposethierry.data.custom_structures.ResultOf
+import com.example.testcomposethierry.data.models.DataArtDetail
+import com.example.testcomposethierry.data.models.DataArtElement
+import com.example.testcomposethierry.data.repositories.ArtDataRepository
+import javax.inject.Inject
+
+class RefetchArtDetailUseCase @Inject constructor(
+    private val artDataRepository: ArtDataRepository,
+) {
+    suspend operator fun invoke(rowId: Int, stateListArt: LazyPagingItems<DataArtElement>, mapArtDetail: MutableMap<Int, DataArtDetail>): DataArtDetail? {
+        val itemData = stateListArt.itemSnapshotList[rowId]
+        itemData?.objectNumber?.let { objectNumber ->
+            when (val resultDetail = artDataRepository.getArtObjectDetail(objectNumber)) {
+                is ResultOf.Success -> {
+                    mapArtDetail.getOrPut(rowId) {
+                        resultDetail.value
+                    }
+                    return resultDetail.value
+                }
+                else -> Unit
+            }
+        }
+        return null
+    }
+}
