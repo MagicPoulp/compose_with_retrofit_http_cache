7853ac38636def91e89434b5837d72ffe99cafb8
Thierry Vilmart <3144141+MagicPoulp@users.noreply.github.com>
2024-05-01 12:03:20 +0200

    add doc

--- app/src/main/java/com/example/testcomposethierry/data/config/Constants.kt
+++ app/src/main/java/com/example/testcomposethierry/data/config/Constants.kt
@@ -3,5 +3,6 @@ package com.example.testcomposethierry.data.config
 object AppConfig {
     const val artBaseUrl = "https://randomuser.me/"
     const val pagingSize = 16
+    // we need a seed to simulate pages so that when we scroll, we keep the same data
     const val seed = "merlin"
 }
